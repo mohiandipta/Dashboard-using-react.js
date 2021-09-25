fcb2f63e9a384dae0f72f76b8b78e9cfb3ef33b1
mohiandipta <mohianmostofa@gmail.com>
2021-09-26 00:00:43 +0600

    styles files added with css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Dashboard from './containers/Dashboard';
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyles } from './styles/global';
+import { lightTheme, darkTheme } from './styles/theme';
 
 const App = () => {
   return (
-    <div>
+    <ThemeProvider theme={lightTheme}>
+      <GlobalStyles />
       <Dashboard />
-    </div>
+    </ThemeProvider>
   );
 }
 
